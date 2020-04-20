87386baf0560f7125bf7683c8aafbd7988ffda65
Toni-Ty <antonique2479@yahoo.com>
2020-04-20 02:11:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 
 function wakeDog(dogName, dogBreed) {
-let string = `Wake ${dogName} the ${dogBreed}`; 
+let string = `Wake ${dogName} the ${dogBreed}`;
 console.log(string);
 return string
 }
 
 function leashDog(dogName, dogBreed) {
-let string = `Leash ${dogName} the ${dogBreed}`; 
+let string = `Leash ${dogName} the ${dogBreed}`;
 console.log(string);
 return string
 }
 
 function walkToPark(dogName, dogBreed) {
-let string = `Walk to the park with ${dogName} the ${dogBreed}`; 
+let string = `Walk to the park with ${dogName} the ${dogBreed}`;
 console.log(string);
 return string
 }
 
 function throwFrisbee(dogName, dogBreed) {
-let string = `Throw the frisbee for ${dogName} the ${dogBreed}`; 
+let string = `Throw the frisbee for ${dogName} the ${dogBreed}`;
 console.log(string);
 return string
 }
 
 function walkHome(dogName, dogBreed) {
-let string = `Walk home with ${dogName} the ${dogBreed}`; 
+let string = `Walk home with ${dogName} the ${dogBreed}`;
 console.log(string);
 return string
 }
 
 function unleashDog(dogName, dogBreed) {
-let string = `Unleash ${dogName} the ${dogBreed}`; 
+let string = `Unleash ${dogName} the ${dogBreed}`;
 console.log(string);
 return string
 }
